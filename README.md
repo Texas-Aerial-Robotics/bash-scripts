b1aa590a766ce987672fa401cb37421ae151883b
daniel <danielespinozarubio@gmail.com>
2018-10-27 15:17:02 -0400

    updates

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hi there welcome to my Bash Scripts
+
+## Possible problems and fixes
+
+> Do you not know how to run a Bash Script?
+
+**Executing properly**
+'''
+source filename.sh
+
+'''
+
+> Do you not want to do that each time
--- run_sim.sh
+++ run_sim.sh
@@ -2,8 +2,8 @@
 
 
 echo "BEGIN"
-gnome-terminal -e  ./sim_run/term1.sh &
-gnome-terminal -e  ./sim_run/term2.sh &
+gnome-terminal  --tab -e  ./sim_run/term1.sh &
+gnome-terminal  --tab -e ./sim_run/term2.sh &
 gnome-terminal -e  ./sim_run/term3.sh &
 wait 
 echo " LOADED"
