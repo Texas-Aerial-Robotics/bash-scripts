e85613d186ac5c9c53a7f59542d790a1802cd6cb
RICHARDOIII <32181854+RICHARDOIII@users.noreply.github.com>
2019-09-19 15:55:46 -0500

    Update rosinstall.sh

--- installs/rosinstall.sh
+++ installs/rosinstall.sh
@@ -27,7 +27,7 @@ echo " INSTALL"
 
 echo "-----------------"
 
-sudo apt-get install ros-melodic-desktop-full -y
+yes | sudo apt-get install ros-melodic-desktop-full
 
 echo "INITIALIZE"
 
@@ -41,4 +41,4 @@ echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 echo " DEPENDENCIES "
 
-sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+yes | sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
