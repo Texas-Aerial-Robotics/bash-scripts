6b6dda9ea59150b9b3de66f167f58e30c6144f91
RICHARDOIII <32181854+RICHARDOIII@users.noreply.github.com>
2019-09-19 15:57:18 -0500

    Update simve_setup.sh

--- installs/simve_setup.sh
+++ installs/simve_setup.sh
@@ -17,8 +17,8 @@ cd ardupilot
 git checkout Copter-3.5
 git submodule update --init --recursive
 
-sudo apt install python-matplotlib python-serial python-wxgtk3.0 python-wxtools python-lxml python-scipy python-opencv ccache gawk git python-pip python-pexpect -y
-sudo pip2 install future pymavlink MAVProxy -y
+yes | sudo apt install python-matplotlib python-serial python-wxgtk3.0 python-wxtools python-lxml python-scipy python-opencv ccache gawk git python-pip python-pexpect
+yes | sudo pip2 install future pymavlink MAVProxy
 
 PATH=/opt/ros/melodic/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/ardupilot/Tools/autotest
 
